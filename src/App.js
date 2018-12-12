6903fd253709bdf57919e7f705da917707a7e614
matthew-helmstetter <mhelmstetter@berkeley.edu>
2018-12-11 21:35:09 -0800

    finishing up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            board : Array(9).fill(null),
+            player : "X",
+            winner : null
+        }
+    }
+
+    checkWinner() {
+        let winLines = 
+        [
+            ["0","1","2"],
+            ["3","4","5"],
+            ["6","7","8"],
+            ["0","3","6"],
+            ["1","4","7"],
+            ["2","5","8"],
+            ["0","4","8"],
+            ["2","4","6"],
+        ]
+
+        for (let index = 0; index < winLines.length; index++) {
+            const [a,b,c] = winLines[index];
+            if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c]) {
+                alert('You won');
+                this.setState({
+                    winner: this.state.player
+                })
+            }
+        }
+    }
+
+    handleClick(index) {
+        let newBoard = this.state.board
+        if(this.state.board[index] === null && !this.state.winner) {
+            newBoard[index] = this.state.player
+            this.setState({
+            board : newBoard,
+            player : this.state.player === "X" ? "O" : "X"
+            })
+
+        }
+        this.checkWinner()
+    }
+    render() {
+        const Box = this.state.board.map(
+            (box, index) => 
+            <div className="box" 
+            key={index} 
+            onClick={() => this.handleClick(index)}>
+            {box}
+            </div>
+        )
+        return (
+            <div className="container">
+                <h1>Tic Tac Toe App</h1>
+                <div className="board">
+                    {Box} 
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default App;
